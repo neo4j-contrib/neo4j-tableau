45ff240ac9e2abe1846ae2f4dd6195a482737d30
Kent Brockman <brockman.ks@gmail.com>
2019-03-06 17:28:34 -0700

    update readme with docker instructions, fixes in package.json, revert some mistakes

--- server.js
+++ server.js
@@ -11,14 +11,19 @@ if (!process.env.CERTIFICATE) errors.push('CERTIFICATE env var not defined');
 if (!process.env.PRIVATE_KEY) errors.push('PRIVATE_KEY env var not defined');
 
 if (errors.length > 0) {
-  throw new Error(`Failed with the following errors:${errors.join(', ')}`);
+	throw new Error(`Failed with the following errors:${errors.join(', ')}`);
 }
 
 var key = fs.readFileSync(process.env.PRIVATE_KEY).toString();
 var cert = fs.readFileSync(process.env.CERTIFICATE).toString();
 let port = process.env.PORT || 3000;
-https.createServer({
-  cert,
-  key
-}, app).listen(port);
+https
+	.createServer(
+		{
+			cert,
+			key
+		},
+		app
+	)
+	.listen(port);
 console.log(`Listening on ${port}`);
