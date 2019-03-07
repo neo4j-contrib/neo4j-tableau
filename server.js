ed5769641979fa5f4d1b356ab82d14ea10669300
Kent Brockman <brockman.ks@gmail.com>
2019-03-07 11:25:08 -0700

    support HTTP by default, small tweaks to README and package.json

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 const fs = require('fs');
+const http = require('http');
 const https = require('https');
-
 const express = require('express');
 
 let app = express();
-app.use('/', express.static('website'));
+let options = {
+	// default page should be Neo4jWdc2.html
+	index: 'Neo4jWdc2.html'
+};
+app.use('/', express.static('website', options));
 
-let errors = [];
-if (!process.env.CERTIFICATE) errors.push('CERTIFICATE env var not defined');
-if (!process.env.PRIVATE_KEY) errors.push('PRIVATE_KEY env var not defined');
-
-if (errors.length > 0) {
-	throw new Error(`Failed with the following errors:${errors.join(', ')}`);
-}
-
-var key = fs.readFileSync(process.env.PRIVATE_KEY).toString();
-var cert = fs.readFileSync(process.env.CERTIFICATE).toString();
+let server;
+let on_https = false;
+var key = process.env.PRIVATE_KEY ? fs.readFileSync(process.env.PRIVATE_KEY).toString() : undefined;
+var cert = process.env.CERTIFICATE ? fs.readFileSync(process.env.CERTIFICATE).toString() : undefined;
 let port = process.env.PORT || 3000;
-https
-	.createServer(
+
+// try HTTPS, otherwise go with HTTP
+if (key && cert) {
+	console.log('key and cert detected, use HTTPS');
+	on_https = true;
+	server = https.createServer(
 		{
 			cert,
 			key
 		},
 		app
-	)
-	.listen(port);
-console.log(`Listening on ${port}`);
+	);
+} else {
+	console.log('key and cert not detected, use HTTP');
+	server = http.createServer(app);
+}
+
+server.listen(port);
+if (on_https)
+	console.log(
+		'Because listening on HTTPS, you may need to access the server on a different domain. Set your DNS accordingly!'
+	);
+console.log(`Listening on ${on_https ? 'https' : 'http'}://localhost:${port}`);
